fcca4d96fab8fc924c61b991bfe80017371de76c
Pieter Stokkink <p.stokkink@amsterdam.nl>
2026-09-07 16:08:30 +0200

    fix tests

--- src/tests/settings.py
+++ src/tests/settings.py
@@ -1,3 +1,5 @@
+from json import loads
+
 from vertrouwelijke_data_proxy.settings import *  # noqa: F403, F405
 
 # The reason the settings are defined here, is to make them independent
@@ -18,7 +20,7 @@ SESSION_COOKIE_SECURE = False
 jwks_key = Path(__file__).parent.parent.joinpath("jwks_test.json").read_text()
 
 DATAPUNT_AUTHZ = {
-    "JWKS": jwks_key,
+    "TRUSTED_JWKS": [{"jwks": loads(jwks_key), "claims": {"iss": "iss"}}],
     "ALWAYS_OK": False,
     "MIN_INTERVAL_KEYSET_UPDATE": 30 * 60,  # 30 minutes
 }
